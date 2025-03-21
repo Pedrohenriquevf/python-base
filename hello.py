b0b9d2fc0561307d28a213ee6d1f00eee3a21f3e
Pedro Franzoi <ph.franzoi@gmail.com>
2025-03-20 23:25:32 -0300

    Programa de Hello, world em varias linguas

--- hello.py
+++ hello.py
@@ -2,6 +2,21 @@
 
 # Este programa imprime Hello, world!
 
-print("Hello, world!")
+# Hello, world - multi linguas
+
+__version__ = "0.01"
+__author__ = "Pedro Franzoi"
+__license__ = "Unlicense"
+
+current_language = os.getenv("LANG")[:5]
+
+msg = "Hello, world!"
+
+if current_language == "pt.BR":
+    msg = "Olá, mundo!"
+elif  current_language == "it.IT":
+    msg = "Ciao, mondo!"
+
+print (msg)
 
 
