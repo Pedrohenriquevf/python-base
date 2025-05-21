c5217c66fc37aae4ebd2c334090f07b9c12d5470
Pedro Henrique <ph.franzoi@gmail.com>
2025-05-20 22:46:49 -0300

    Incluso conjunto SET para melhorar o programa - escola.py

--- escola.py
+++ escola.py
@@ -5,7 +5,7 @@ Imprimir a lista de criança agrupadas por sala
 que frequentam cada uma das atividades.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # Dados
 sala1 = ["Erick", "Maria", "Pedro", "Jóse", "Fernanda"]
@@ -28,14 +28,17 @@ for nome_atividade, atividade in atividades:
     print (f"Alunos da atividade, {nome_atividade}\n")
     print ("-" * 40)
 
-    atividade_sala1 = []
-    atividade_sala2 = []
+    atividade_sala1 = set(sala1) & set(atividade)
+    atividade_sala2 = set(sala2) & set(atividade)
 
-    for aluno in atividade: 
-        if aluno in sala1:
-            atividade_sala1.append(aluno)
-        elif aluno in sala2:
-            atividade_sala2.append(aluno)
+    # Sala 1 que tem interseção com a atividade
+
+    
+   # for aluno in atividade: 
+       # if aluno in sala1:
+           # atividade_sala1.append(aluno)
+       # elif aluno in sala2:
+           # atividade_sala2.append(aluno)
 
     print(f"{nome_atividade} sala1 ", atividade_sala1)
     print(f"{nome_atividade} sala2 ", atividade_sala2)
